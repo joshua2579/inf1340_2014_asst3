40b857f73e0101e8b0539a152fed7dc3de74d7f5
mmkrisi <kristina.mitova@gmail.com>
2014-11-19 12:24:14 -0500

    Started writing mining.py code

--- mining.py
+++ mining.py
@@ -12,14 +12,41 @@ __status__ = "Prototype"
 
 # imports one per line
 import datetime
-
-stock_data = []
-monthly_averages = []
+from StockMiner import StockMiner
 
 
 def read_stock_data(stock_name, stock_file_name):
-    return
+    """
 
+    :param stock_name:
+    :param stock_file_name:
+    :return:
+    """
+
+    global stock
+    stock = StockMiner(stock_name, stock_file_name)
+    stock.monthly_averages()
+
+
+def six_months(sort_order):
+    """
+
+    :param sort_order:
+    :return:
+    """
+
+    results = []
+    list_averages = []
+    if sort_order == "descending":
+        sort_order = False
+    elif sort_order == "ascending":
+        sort_order = True
+    else:
+        raise ValueError("Unexpected sort order.")
+
+    for sales_average in stock.monthly_averages.values():
+        list_averages.append(sales_average)
+    list_averages.sort(reverse=sort_order)
 
 def six_best_months():
     return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
